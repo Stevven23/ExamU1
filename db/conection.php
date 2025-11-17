876b2737ecf3ba24321c506402fc3dfafa75ede6
Michael Simbaña <michaelrivadeneira99@gmail.com>
2025-11-17 18:10:07 -0500

    Fix Connection

--- db/conection.php
+++ db/conection.php
@@ -1,19 +1,23 @@
 <?php
 $host = 'db.fr-pari1.bengt.wasmernet.com';
-$dbname = 'songs24_db';
-$username = '
-b45b17d8793580004ebd2387cf82';
-$password = '0691b45b-17d8-7a7d-8000-32edf125002a';
+$dbname = 'song34_db';
+$username = 'b520f9db75d7800076e7811fda06';
+$password = '0691b520-f9db-7887-8000-a6360d58b1ee';
 $charset = 'utf8';
 
-$port = 3306;
+// ¡El puerto es 10272, no 3306!
+$port = 10272; 
+
 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
 
-
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $username, $password, $options);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
